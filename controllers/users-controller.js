e65736d43be1162004972b03e9913ddec1e30c59
Kat Reinhart <kat.reinhart@gmail.com>
2017-12-21 10:30:49 -0800

    Start adding auth routes

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,14 +1,55 @@
 const Model = require('../models/users-model.js')
 console.log('controller here');
 
+const fields = ['email', 'password']
+
+
 class UsersController {
 
-  static getAll(req, res, next){
-    Model.all().then(response => {
-      res.json({  })
+  // static getAll(req, res, next){
+  //   Model.all().then(response => {
+  //     res.json({  })
+  //   })
+  // }
+
+  static emailIsUnique(req, res, next) {
+    Model.exists(req.body.email).then(exists => {
+      next({ error: 'User already exists'})
+    }).catch(err => {
+      next()
     })
   }
 
+  static fieldsExist(req, res, next) {
+    const errors = []
+    fields.forEach(field => {
+      if(!req.body.hasOwnProperty(field)) {
+        errors.push(`${field} is required`)
+      } 
+    })
+
+    if(errors.length) {
+      next({ status: 400, message: 'There were errors', errors})
+    } 
+    else {
+      next()
+    }
+  }
+
+  static prune(req, res, next) {
+    Object.keys(req.body).forEach(key => {
+      if(!fields.includes(keys)) {
+        delete req.body.key
+      }
+    })
+    next()
+  }
+
+  static createNewUser(req, res, next) {
+    Model.signup(req.body).then(res => {
+      res.status(200).json({ })
+    })
+  }
 }
 
 module.exports = UsersController
--- models/users-model.js
+++ models/users-model.js
@@ -1,13 +1,39 @@
 const db = require('../db/connection.js')
 console.log('model here');
+const bcrypt = require('bcrypt-as-promised')
+const jwt = require('jsonwebtoken')
+const Token = require('./token-model')
 
 
 class UsersModel {
 
-  static all(){
-    return db(users)
+  // static all(){
+  //   return db('users')
+  // }
+
+  static signup (email, password) {
+    return db('users').where({ email }).first()
+      .then(user => {
+        if(user) throw new Error()
+        const passhash = bcrypt.hashSync(password, 8)
+
+        return User.create({ email, passhash })
+      })
+      .catch(() => { throw new Error('User signup failed') })
   }
 
+  static login (email, password) {
+    return db('users').where({ email })
+      .then(user => {
+        if(bcrypt.compareSync(password, user[0].passhash)) {
+          const sub = { id: user[0].id, email: user[0].email }
+          return Token.signToken(sub)          
+        } else {
+          throw new Error('Login failed')
+        }
+      })
+      .catch(() => { throw new Error('Login failed') })
+  }
 }
 
 
