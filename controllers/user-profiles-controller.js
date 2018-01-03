70fc180c1e7162991e477ab1db745d889ce3d85b
Kat Reinhart <kat.reinhart@gmail.com>
2018-01-03 13:41:08 -0800

    Debug add steps on backend

--- controllers/user-profiles-controller.js
+++ controllers/user-profiles-controller.js
@@ -28,12 +28,12 @@ class userProfilesController {
     Model.createUserProfile(body).then( response => {
       // console.log('response',response);
 
-      res.json({response})
+      res.status(200).json({response})
     })
   }
 
   static updateUserProfile(req, res, next) {
-    console.log('updating user profile', req.body)
+    console.log('controller updating user profile', req.body)
     const body = {
       user_id: req.body.user_id,
       plant_types_id: req.body.plant_types_id
@@ -41,7 +41,11 @@ class userProfilesController {
 
     PlantInstanceModel.createNewInstance(body).then(result => {
       console.log(result)
-      res.status(200).json({result})
+      const id = result[0].id
+      Model.updateUserProfile(body.user_id, {plant_instances_id: id}).then(result => {
+        console.log(result)
+        res.status(200).json({result})
+      })
     })
   }
 }
