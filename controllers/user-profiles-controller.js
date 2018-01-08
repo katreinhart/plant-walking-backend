15fdbf247be66470a8dca83e0f3f9345a1b011ae
Wes Reid <bwreid@gmail.com>
2018-01-08 14:48:43 -0800

    Update user-profiles-controller.js

--- controllers/user-profiles-controller.js
+++ controllers/user-profiles-controller.js
@@ -6,44 +6,26 @@ class userProfilesController {
   static getOneUserProfile(req, res, next) {
     let id = req.params.id
     Model.getOneUserProfile(id).then(response => {
-      if(response.length){
+      if(!response.length) return next({error: 'User not found :(  '})
       res.json({response})
-      }
-      else{
-        next({error: 'User not found :(  '})
-      }
     })
   }
 
   static createUserProfile(req, res, next){
-    // console.log('creatingUP', req.body);
-    const body = {
-      user_id: req.body.user_id,
-      display_name:req.body.display_name,
-      plant_instances_id: req.body.plant_instances_id,
-      is_admin: false,
-    }
-    // console.log('body', body);
+    const { user_id, display_name, plant_instances_id } = req.body
+    const body = { user_id, display_name, plant_instances_id, is_admin: false }
 
     Model.createUserProfile(body).then( response => {
-      // console.log('response',response);
-
       res.status(200).json({response})
     })
   }
 
   static updateUserProfile(req, res, next) {
-    console.log('controller updating user profile', req.body)
-    const body = {
-      user_id: req.body.user_id,
-      plant_types_id: req.body.plant_types_id
-    }
+    const { user_id, plant_types_id } = req.body
 
-    PlantInstanceModel.createNewInstance(body).then(result => {
-      console.log(result)
+    PlantInstanceModel.createNewInstance({ user_id, plant_types_id }).then(result => {
       const id = result[0].id
       Model.updateUserProfile(body.user_id, {plant_instances_id: id}).then(result => {
-        console.log(result)
         res.status(200).json({result})
       })
     })
