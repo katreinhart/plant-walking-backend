9d0118f9898468bb01740c77c877ef2411242a31
Wes Reid <bwreid@gmail.com>
2018-01-08 14:39:53 -0800

    Update user-profiles-model.js

--- models/user-profiles-model.js
+++ models/user-profiles-model.js
@@ -4,21 +4,17 @@ const db = require('../db/connection.js')
 class userProfilesModel {
 
   static getOneUserProfile(id){
-    // console.log('user', id);
     return db('user_profiles')
     .where({id})
   }
 
   static createUserProfile(body){
-    // console.log('Model body',body);
     return db('user_profiles')
     .insert(body)
     .returning('*')
   }
 
   static updateUserProfile(id, body) {
-    console.log('Model user profile update')
-    console.log(body)
     return db('user_profiles')
       .where({ user_id: id })
       .update(body)
